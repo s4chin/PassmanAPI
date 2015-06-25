7157d8c2d22661dca593dacf2efa3dff2146b556
Sachin Kelkar <sachinkel19@gmail.com>
2015-06-25 16:19:18 +0530

    Create base

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/<service>/<password>', methods=['GET'])
+def respond(service, password):
+    return jsonify({'password': 'passw0rD'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
